dbc90658e6621e2a5a2cee54ed1ea6792d58c99a
Petar Kirilov Georgiev <pkirilov@brte.boeing.es>
2018-04-11 14:55:31 +0200

    First commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <text class="message">Nowwww {{ name }} , let's use Vue.js to build your Weex app.</text>
+</template>
+
+<script>
+  export default {
+    name: 'list',
+    data () {
+      return {
+        name: "test"
+        /*logo: 'https://gw.alicdn.com/tfs/TB1yopEdgoQMeJjy1XaXXcSsFXa-640-302.png'*/
+      }
+    }
+  }
+</script>
+
+<style>
+  .message {
+    margin: 30px;
+    font-size: 32px;
+    color: #727272;
+  }
+</style>
